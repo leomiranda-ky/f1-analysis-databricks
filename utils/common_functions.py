fb9c38123fcbb96e31c74df3b2889b07749d94b1
leonardomiranda125@hotmail.com <leonardomiranda125@hotmail.com>
2025-09-07 22:58:37 +0000

    feat(transformation): add transformations to processed data

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,7 +1,12 @@
 from pyspark.sql.functions import current_timestamp
 
-
 def add_ingestion_date(input_df):
     output_df = input_df.withColumn("ingestion_date", current_timestamp())
     return output_df
 
+
+def df_column_to_list(input_df, column_name):
+    df_row_list = input_df.select(column_name).distinct().collect()
+
+    column_value_list = [row[column_name] for row in df_row_list]
+    return column_value_list
